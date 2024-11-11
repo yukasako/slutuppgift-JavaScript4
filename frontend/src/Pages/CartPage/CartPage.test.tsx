00332f559148a8fa807c18fe03ff19218f65402c
yukasako <yukako.sakozono@yh.nackademin.se>
2024-11-11 12:49:48 +0100

    Add test-script on package.json.

--- frontend/src/Pages/CartPage/CartPage.test.tsx
+++ frontend/src/Pages/CartPage/CartPage.test.tsx
@@ -2,7 +2,7 @@ import { describe, it, beforeEach } from 'vitest'; // expect missing
 import { render } from '@testing-library/react'; // screen missing
 import CartPage from './CartPage';
 
-beforeEach(() => render(<CartPage></CartPage>));
+beforeEach(() => render(<CartPage />));
 
 describe('CartPage', () => {
   it('Render products in the cart with correct quantity and price.', () => {
