7f9c8f44495b41db3c8331dcd07cdc62300c61d4
yukasako <yukako.sakozono@yh.nackademin.se>
2024-11-11 15:23:52 +0100

    Install Tailwind.

--- frontend/src/Components/HeaderFooter/Header.tsx
+++ frontend/src/Components/HeaderFooter/Header.tsx
@@ -1,5 +1,25 @@
+import { Link } from 'react-router-dom';
+
 function Header() {
-  return <div>Header</div>;
+  return (
+    <div className='bg-transparent z-10 flex flex-row justify-between px-6 py-4'>
+      <img src='' alt='logo' />
+      <nav className='flex flex-row space-x-6'>
+        <div>
+          <i></i>
+          <Link to={'/'}>Home</Link>
+        </div>
+        <div>
+          <i></i>
+          <Link to={'/products'}>Products</Link>
+        </div>
+        <div>
+          <i></i>
+          <Link to={'/cart'}>Cart</Link>
+        </div>
+      </nav>
+    </div>
+  );
 }
 
 export default Header;
