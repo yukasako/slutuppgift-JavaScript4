4d0ea26cb923474e9b8e75fa2a6cc9d9a39c41eb
yukasako <yukako.sakozono@yh.nackademin.se>
2024-11-13 14:23:23 +0100

    Install FontAwesome.

--- frontend/src/Components/HeaderFooter/Header.tsx
+++ frontend/src/Components/HeaderFooter/Header.tsx
@@ -1,22 +1,27 @@
 import { Link } from 'react-router-dom';
 import logo from '../../assets/logo/logo.png';
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHouseChimney } from '@fortawesome/free-solid-svg-icons';
+import { faGift } from '@fortawesome/free-solid-svg-icons';
+import { faSleigh } from '@fortawesome/free-solid-svg-icons';
+
 function Header() {
   return (
     <div className='relative z-10 px-10'>
       <div className='bg-transparent text-white flex flex-row justify-between py-4'>
         <img src={logo} alt='logo' className='h-10' />
-        <nav className='flex flex-row space-x-6 items-center font-bold px-10'>
-          <div>
-            <i></i>
+        <nav className='flex flex-row items-center gap-6 font-bold px-10'>
+          <div className='flex flex-row items-center gap-2'>
+            <FontAwesomeIcon icon={faHouseChimney} />
             <Link to={'/'}>Home</Link>
           </div>
-          <div>
-            <i></i>
+          <div className='flex flex-row items-center gap-2'>
+            <FontAwesomeIcon icon={faGift} />
             <Link to={'/products'}>Products</Link>
           </div>
-          <div>
-            <i></i>
+          <div className='flex flex-row items-center gap-2'>
+            <FontAwesomeIcon icon={faSleigh} />
             <Link to={'/cart'}>Cart</Link>
           </div>
         </nav>
