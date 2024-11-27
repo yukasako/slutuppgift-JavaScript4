abec6d80a6a987b50ec6bf744cf37d5297149dba
yukasako <yukako.sakozono@yh.nackademin.se>
2024-11-27 10:55:17 +0100

    Implement cart.

--- frontend/src/Components/CartItemCard.tsx
+++ frontend/src/Components/CartItemCard.tsx
@@ -1,3 +1,4 @@
+import { useState, useEffect } from 'react';
 import { CartItemModel } from '../Models/ItemModel';
 
 type CartItemProps = {
@@ -5,6 +6,19 @@ type CartItemProps = {
 };
 
 function CartItemCard({ item }: CartItemProps) {
+  const [quantity, setQuantity] = useState(item.quantity);
+  const [totalPrice, setTotalPrice] = useState(item.price * item.quantity);
+
+  useEffect(() => {
+    // UI
+    setTotalPrice(quantity * item.price);
+    // Backend
+    updateQuantity();
+    if (quantity === 0) {
+      deleteCartItem();
+    }
+  }, [quantity]);
+
   const deleteCartItem = async () => {
     try {
       const response = await fetch(`http://localhost:3001/cart/${item.id}`, {
@@ -19,28 +33,72 @@ function CartItemCard({ item }: CartItemProps) {
     }
   };
 
+  const updateQuantity = async () => {
+    try {
+      const updateQuantity = {
+        ...item,
+        quantity: quantity,
+      };
+      const response = await fetch(`http://localhost:3001/cart/${item.id}`, {
+        method: 'PUT',
+        headers: {},
+        body: JSON.stringify(updateQuantity),
+      });
+      if (response.ok) {
+        console.log(updateQuantity.quantity);
+      }
+    } catch (err) {
+      alert(`Error: ${err}`);
+    }
+  };
+
+  const increment = () => {
+    setQuantity(quantity + 1);
+  };
+  const decrement = () => {
+    if (quantity !== 0) {
+      setQuantity(quantity - 1);
+    }
+  };
+
   return (
-    <div className='flex flex-row mx-auto gap-10 lg:gap-24'>
+    <div className='max-w-lg flex flex-row mx-auto gap-10 lg:gap-24 p-3 shadow-lg'>
       <img
         src={`/productImages/${item.image}.webp`}
         alt='productImg'
-        className='w-1/3'
+        className='w-1/3 object-contain'
       />
-      <div className='flex flex-col px-2'>
-        <p className='font-bold'>{item.name}</p>
-        <p className=''>{item.price} kr</p>
-        <div>
-          <button>-</button>
-          <span>{item.quantity}</span>
-          <button>+</button>
+      <div className='w-2/3 flex flex-col p-3 gap-3'>
+        <p className='font-bold text-lg'>{item.name}</p>
+        <p className='text-right'>Unit Price: {item.price} kr</p>
+        <div className='flex flex-row gap-3 justify-end items-center'>
+          <span>Quantity: </span>
+          <button
+            className='w-6 h-6 bg-neutral-200 text-black rounded-sm'
+            onClick={() => {
+              decrement();
+            }}
+          >
+            -
+          </button>
+          <span>{quantity}</span>
+          <button
+            className='w-6 h-6 bg-neutral-200 text-black rounded-sm'
+            onClick={() => {
+              increment();
+            }}
+          >
+            +
+          </button>
         </div>
+        <p className='text-right'>Total: {totalPrice}kr</p>
         <button
           onClick={() => {
             deleteCartItem();
           }}
-          className='bg-black text-white rounded-md'
+          className='bg-neutral-200 text-black rounded-full p-1'
         >
-          Delete
+          Remove from cart
         </button>
       </div>
     </div>
--- frontend/src/Pages/CartPage/CartPage.tsx
+++ frontend/src/Pages/CartPage/CartPage.tsx
@@ -5,25 +5,43 @@ import { CartItemModel } from '../../Models/ItemModel';
 
 function CartPage() {
   const [cart, setCart] = useState<CartItemModel[]>([]);
+  const [totalQuantity, setTotalQuantity] = useState(0);
+  const [totalPrice, setTotalPrice] = useState(0);
 
   useEffect(() => {
     getCartItems();
-  }, []);
+    caluculateTotal();
+  }, [cart]);
 
   const getCartItems = async () => {
     const cartItems = await fetchData('cart');
     setCart(cartItems);
   };
 
+  const caluculateTotal = () => {
+    let totalPrice = 0;
+    let totalQuantity = 0;
+    cart.forEach((item) => {
+      const itemTotalPrice = item.price * item.quantity;
+      totalPrice += itemTotalPrice;
+      totalQuantity += item.quantity;
+      console.log('Total:', totalPrice);
+    });
+    setTotalPrice(totalPrice);
+    setTotalQuantity(totalQuantity);
+  };
+
   return (
-    <div>
-      <div>
+    <div className='w-4/5 flex flex-col md:flex-row item-center justify-around mx-auto gap-12 py-12'>
+      <div className='flex flex-col gap-10 border-'>
         {cart.map((item, i) => (
           <CartItemCard item={item} key={i} />
         ))}
       </div>
-      <div>
+      <div className='flex flex-col'>
         <h1>Cart Items</h1>
+        <p>Total Quantity: {totalQuantity}</p>
+        <p>Total price: {totalPrice}</p>
       </div>
     </div>
   );
