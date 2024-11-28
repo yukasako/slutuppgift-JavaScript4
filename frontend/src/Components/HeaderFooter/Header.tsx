221e0d53e090f5d3161407e6e28a107446128a02
yukasako <yukako.sakozono@yh.nackademin.se>
2024-11-28 22:18:47 +0100

    Update style on nav. Add project setup instruction on README.md.

--- frontend/src/Components/HeaderFooter/Header.tsx
+++ frontend/src/Components/HeaderFooter/Header.tsx
@@ -5,21 +5,69 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faHouseChimney } from '@fortawesome/free-solid-svg-icons';
 import { faGift } from '@fortawesome/free-solid-svg-icons';
 import { faSleigh } from '@fortawesome/free-solid-svg-icons';
+import { faBars } from '@fortawesome/free-solid-svg-icons/faBars';
+import { useState } from 'react';
 
 function Header(props: { location: string }) {
-  // console.log(props.location);
+  const [mobileNav, setMobileNav] = useState('hidden');
+
+  const toggleShowNav = () => {
+    if (mobileNav === 'flex') {
+      setMobileNav('hidden');
+    } else {
+      setMobileNav('flex');
+    }
+  };
+
   let bgColor = 'bg-transparent';
   if (props.location !== '/') {
     bgColor = 'bg-gradient-to-r from-red-800 to-red-950';
-    // bgColor = 'bg-black';
   }
+
   return (
     <div className='relative z-10'>
       <div
         className={`${bgColor} text-white flex flex-row justify-between py-4 px-8`}
       >
-        <img src={logo} alt='logo' className='h-10' />
-        <nav className='flex justify-end gap-6 font-bold'>
+        <Link to={'/'}>
+          <img src={logo} alt='logo' className='h-10' />
+        </Link>
+        {/* Humburger Btn */}
+        <button
+          className='sm:hidden z-30'
+          onClick={() => {
+            toggleShowNav();
+          }}
+        >
+          <FontAwesomeIcon icon={faBars} className='h-8' />
+        </button>
+        {/* Desktop Nav */}
+        <nav className='hidden sm:flex justify-end gap-6 font-bold'>
+          <Link to={'/'} className='flex flex-row items-center gap-2'>
+            <FontAwesomeIcon icon={faHouseChimney} />
+            <span>Home</span>
+          </Link>
+          <Link to={'/products'} className='flex flex-row items-center gap-2'>
+            <FontAwesomeIcon icon={faGift} />
+            <span>Products</span>
+          </Link>
+          <Link to={'/cart'} className='flex flex-row items-center gap-2'>
+            <FontAwesomeIcon icon={faSleigh} />
+            <span>Cart</span>
+          </Link>
+        </nav>
+        {/* Mobile Nav */}
+        <nav
+          className={`${mobileNav} sm:hidden 
+        w-full h-dvh 
+        bg-black text-white opacity-80 font-bold 
+        absolute z-20 top-0 right-0
+        flex flex-col items-end px-10 py-32 gap-6 
+        `}
+          onClick={() => {
+            toggleShowNav();
+          }}
+        >
           <Link to={'/'} className='flex flex-row items-center gap-2'>
             <FontAwesomeIcon icon={faHouseChimney} />
             <span>Home</span>
--- frontend/src/Pages/HomePage/HomePage.tsx
+++ frontend/src/Pages/HomePage/HomePage.tsx
@@ -38,7 +38,7 @@ function HomePage() {
           >
             Christmas Featured Collection
           </h1>
-          <Link to={'/products'} className='z-10'>
+          <Link to={'/products'}>
             <button className='bg-white text-black px-6 py-3 rounded-md font-bold'>
               Check Collection
             </button>
